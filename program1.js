f22c0f1b79879df8055e3bdaf5c45b99ae5acb9f
mansi-jaimini <mansijaimini31@gmail.com>
2024-02-15 14:55:09 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,7 +4,7 @@
  */
 var isValid = function(s) {
     const stack=[];
-    for(let i=0;i<s.length;i++){
+    for(let i=0;i<stack.length;i++){
         const top=s[s.length-1]
     }
     if(s[i]==='(' )
