f52668406aae0d0de086f1c83103840292372ecb
mansi-jaimini <mansijaimini31@gmail.com>
2024-02-15 14:57:23 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -9,7 +9,7 @@ var isValid = function(s) {
     }
     if(s[i]==='(' || s[i]==='{' ||s[i]==='[')
         stack.push(s[i]);
-    else if(s[i]===')')
+    else if(s[i]===')'|| s[i]==='')
    
     
 };
