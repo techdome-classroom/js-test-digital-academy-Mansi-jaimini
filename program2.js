2db6278d619dff59e91672469019daa25227a3bf
mansi-jaimini <mansijaimini31@gmail.com>
2024-02-15 15:09:54 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let string=('I', 'V', 'X', 'L', 'C', 'D', 'M');
-    
+    let I =1,
+    let V =5 X 10 L 50 C 100 D 500 M 1000 
+
     
 };
 
