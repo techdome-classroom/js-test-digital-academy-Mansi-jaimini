6823a19b70f89d4937000614db330b230dd033cb
mansi-jaimini <mansijaimini31@gmail.com>
2024-02-15 15:25:35 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -5,8 +5,8 @@
 var romanToInt = function(s) {
     const romanMap=new Map();
     romanMap.set('I',1);
-    romanMap.set('I',1);
-    romanMap.set('I',1);
+    romanMap.set('v',5);
+    romanMap.set('x',10);
     romanMap.set('I',1);
     romanMap.set('I',1);
     romanMap.set('I',1);
