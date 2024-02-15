ea671fa49cfb744e7e4bbe154b9ba1dd77d57426
mansi-jaimini <mansijaimini31@gmail.com>
2024-02-15 15:43:25 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,10 +3,19 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    if(str1==null)
-    return -1;
-var num=char_to_int(str1.charAt(0));
-var pre,curr;
+    const romanMap=new Map();
+    romanMap.set('I',1);
+    romanMap.set('V',5);
+    romanMap.set('X',10);
+    romanMap.set('L',50);
+    romanMap.set('C',100);
+    romanMap.set('D',500);
+    romanMap.set('M',1000);
+    const n=s.length();
+    let num=romanMap.get(s. [n-1]);
+    for(let i=n-2;i>0;i--){
+
+    }
 
 
     
