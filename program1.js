0facadaae6733f6ba4581d6ef84c831da0d59ebe
mansi-jaimini <mansijaimini31@gmail.com>
2024-02-15 15:40:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,25 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const leftSymbols=[];
+    const stack=[];
     for(let i=0;i<stack.length;i++){
-        const top=leftSymbols[leftSymbols.length-1]
+        const top=stack[stack.length-1]
     if(s[i]==='(' || s[i]==='{' ||s[i]==='[')
-        leftSymbols.push(s[i]);
-    else if(s[i]===')' && leftSymbols.length===0&&leftSymbol[leftSymbols.length-1]==='('){
-        leftSymbols.pop();
+        stack.push(s[i]);
+    else if(s[i]===')' && top==='(' && s.length!==0){
+        stack.pop();
     }
-    else if(s[i]==='}' && leftSymbols.length===0&&leftSymbol[leftSymbols.length-1]==='('){
-        leftSymbols.pop();
+    else if(s[i]==='}' && top==='{' && s.length!==0){
+        stack.pop();
     }
     else if(s[i]===']' && top==='[' && s.length!==0){
-        leftSymbols.pop();
+        stack.pop();
     }
     else{
         return false
     }
 }
 return stack.length===0;
+
 };
 
 module.exports = { isValid };
