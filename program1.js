579c0660746b9ddba6a2872ced57d75f01cb7cb8
mansi-jaimini <mansijaimini31@gmail.com>
2024-02-15 15:34:14 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,7 +5,7 @@
 var isValid = function(s) {
     const leftSymbols=[];
     for(let i=0;i<stack.length;i++){
-        const top=leftSynbols[leftSynbols.length-1]
+        const top=leftSymbols[leftSymbols.length-1]
     if(s[i]==='(' || s[i]==='{' ||s[i]==='[')
         leftSynbols.push(s[i]);
     else if(s[i]===')' && top==='(' && s.length!==0){
